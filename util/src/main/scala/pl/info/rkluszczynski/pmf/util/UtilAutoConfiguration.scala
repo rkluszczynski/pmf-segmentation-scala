7e930f66b9ddfbbe07adf4ba538a47cdf00fb530
Rafal Kluszczynski <klusi@icm.edu.pl>
2016-04-02 07:45:38 +0200

    spring factory configuration for util module

---
+++ util/src/main/scala/pl/info/rkluszczynski/pmf/util/UtilAutoConfiguration.scala
@@ -0,0 +1,7 @@
+package pl.info.rkluszczynski.pmf.util
+
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan(Array("pl.info.rkluszczynski.pmf.util"))
+class UtilAutoConfiguration {
+}
